ae9909d5bc257fa36b66a23d5fabe9c14a1cf700
Troy Mullaney <troym9731@gmail.com>
2015-08-18 14:41:41 -0700

    Shorten Header code

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,17 @@ var Header = React.createClass({
   },
 
   checkForSearch: function() {
-    return this.props.search !== '/' ? true : false;
+    if (this.props.search !== '/') {
+      return <a href="/#/search" className="btn">Search</a>;
+    } else {
+      return;
+    }
+  },
+
+  checkForLogin: function() {
+    if (loggedIn) {
+      
+    }
   },
 
   render: function() {
@@ -21,10 +31,7 @@ var Header = React.createClass({
       <header className={headerColor}>
         <a href="/#/"><i className="fa fa-music"></i></a>
         <nav className="primary-nav">
-          {this.checkForSearch() 
-            ? <a href="/#/search" className="btn">Search</a>
-            : ''
-          }
+          {this.checkForSearch()}
           <a href="/#/signup" className="btn">Sign Up</a>
           <a href="/#/login" className="btn">Login</a>
         </nav>
