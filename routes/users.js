074d7c0b14e092a11d3b619ebf475058d3294003
Troy Mullaney <troym9731@gmail.com>
2015-10-02 15:39:44 -0700

    Express work

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+
+// GET users
+router.get('/', function(req, res) {
+  var userCollection = db.get().collection('users').find();
+});
 
 module.exports = router;
